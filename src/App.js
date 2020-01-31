39b1a646f0b2a66898af48103e6d9a4bd293115c
Nan <nanzhang.software@gmail.com>
2020-01-30 20:43:29 -0500

    init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import TodoItem from "./component/TodoItem";
+
+function App() {
+  return (
+      <div>
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+      </div>
+  )
+}
+
+export default App;
---
+++ src/component/TodoItem.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function TodoItem() {
+    return (
+        <div>
+            <input type="checkbox" />
+            <p> Ask for Visa Status </p>
+        </div>
+    )
+}
+
+export default TodoItem
